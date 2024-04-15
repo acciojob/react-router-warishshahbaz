556bb2ce8c27e0757cb7020c22dc7c82e0ed9e41
warishshahbaz <mdshahbazwarish1996@gmail.com>
2024-04-15 17:40:01 +0530

    impl

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This is a simple React Routes program.</p>
+    </div>
+  );
+}
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,33 @@
-
 import React from "react";
-import './../styles/App.css';
+import "./../styles/App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Routes>
+            <Route path="/about" element={<About />} />
 
-export default App
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+};
+
+export default App;
